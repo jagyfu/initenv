39e86be6938c96068b1abd8948b9aba28016662b
jagyfu <xmgwbn@gmail.com>
2021-03-03 01:56:50 +0800

    Update install_bash.sh

--- install_bash.sh
+++ install_bash.sh
@@ -2,24 +2,27 @@
 
 mkdir -p /root/soft/
 cd /root/soft
-if [ ! -f "udp2raw_x86"]; then
+if [ ! -e "udp2raw_x86" ]; then
 ## install udp2raw
   wget https://github.com/wangyu-/udp2raw-tunnel/releases/download/20200818.0/udp2raw_binaries.tar.gz -O udp2raw_binaries.tar.gz
   tar zxvf udp2raw_binaries.tar.gz
   mv udp2raw_x86 udp2raw
+  chmod +x udp2raw_x86
 fi
 
 ## install gost
-if [ ! -f "gost"]; then
+if [ ! -e "gost" ]; then
   wget https://github.com/ginuerzh/gost/releases/download/v2.11.1/gost-linux-386-2.11.1.gz -O gost-linux-386-2.11.1.gz
   gunzip -d gost-linux-386-2.11.1.gz
   mv gost-linux-386-2.11.1 gost
+  chmod +x gost 
 fi 
 
 ## install kcp
-if [ ! -f "server_linux_amd64"]; then
+if [ ! -e "server_linux_amd64" ]; then
   wget https://github.com/xtaci/kcptun/releases/download/v20210103/kcptun-linux-386-20210103.tar.gz -O kcptun-linux-386-20210103.tar.gz
   tar xzvf kcptun-linux-386-20210103.tar.gz
+  chmod +x server_linux_amd64
 fi
 
 ## start kcp 
