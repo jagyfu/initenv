4072acc3522bec6edb238ca0f698f18f8b7212b2
jagyfu <xmgwbn@gmail.com>
2021-03-06 10:46:59 +0800

    Create ddros.sh

---
+++ ddros.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+yum install wget -y
+yum install net-tools -y
+wget https://download.mikrotik.com/routeros/6.47.8/chr-6.47.8.img.zip -O chr.img.zip && \
+gunzip -c chr.img.zip > chr.img && \
+mount -o loop,offset=512 chr.img /mnt && \
+ADDR0=`ip addr show eth0 | grep global | cut -d' ' -f 6 | head -n 1` && \
+GATE0=`ip route list | grep default | cut -d' ' -f 3` && \
+mkdir -p /mnt/rw && \
+echo "/ip address add address=$ADDR0 interface=[/interface ethernet find where name=ether1]
+/ip route add gateway=$GATE0
+" > /mnt/rw/autorun.scr && \
+umount /mnt && \
+echo u > /proc/sysrq-trigger && \
+dd if=chr.img bs=1024 of=/dev/vda && reboot
