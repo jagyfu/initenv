cbd0649feb46d75ffc25a6ee8afddf02ae36e58f
jagyfu <xmgwbn@gmail.com>
2021-07-04 08:47:19 +0800

    Update ddros.sh

--- ddros.sh
+++ ddros.sh
@@ -3,7 +3,7 @@
 yum install wget -y
 yum install net-tools -y
 #wget https://download.mikrotik.com/routeros/6.47.8/chr-6.47.8.img.zip -O chr.img.zip && \
-wget https://download.mikrotik.com/routeros/6.48.2/chr-6.48.2.img.zip -O chr.img.zip && \
+wget https://download.mikrotik.com/routeros/6.48.3/chr-6.48.3.img.zip -O chr.img.zip && \
 gunzip -c chr.img.zip > chr.img && \
 mount -o loop,offset=512 chr.img /mnt && \
 ADDR0=`ip addr show eth0 | grep global | cut -d' ' -f 6 | head -n 1` && \
