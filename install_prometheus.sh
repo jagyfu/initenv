48a3f63c7af33cd72a0e88708530bb2aba463beb
jagyfu <xmgwbn@gmail.com>
2021-09-20 12:53:50 +0800

    Create install_prometheus.sh

---
+++ install_prometheus.sh
@@ -0,0 +1,16 @@
+## 安装prometheus
+
+
+## ubuntu install grafana
+#sudo apt-get install -y adduser libfontconfig1
+#wget https://dl.grafana.com/enterprise/release/grafana-enterprise_8.1.4_amd64.deb
+#sudo dpkg -i grafana-enterprise_8.1.4_amd64.deb
+
+## centos install grafana
+#wget https://dl.grafana.com/enterprise/release/grafana-enterprise-8.1.4-1.x86_64.rpm
+#sudo yum install grafana-enterprise-8.1.4-1.x86_64.rpm
+
+##run grafana 
+#systemctl start grafana-server.service 
+
+## import grafana json https://grafana.com/grafana/dashboards/8919
