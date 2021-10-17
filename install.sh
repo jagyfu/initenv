4841bb713962d2e1dde28ad47bd9ed92642d975f
jagyfu <xmgwbn@gmail.com>
2021-10-17 13:26:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ chmod 777 /home/work/logs/
 mkdir -p ~/soft
 
 ## install git ...
-yum install python python2 net-tools vim git supervisor dstat htop -y
+yum install python python2 python3 net-tools vim git supervisor dstat htop -y
 
 ## install gost
 if [ ! -e "gost" ]; then
